c24b7511077be7a908d1919cb0a13b0ecff3f170
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2019-05-08 22:32:12 +0700

    Update foot.php

--- application/views/profile/_partials/foot.php
+++ application/views/profile/_partials/foot.php
@@ -4,7 +4,7 @@
   <a href="index.html"><img src="<?php echo base_url('images/logo.png')?>" alt=""/></a>
  </div>
 <div class="copy">
-     <p>© 2014 Design by <a href="http://w3layouts.com" target="_blank">w3layouts</a></p>
+     <p>© 2019 Design by Tim Dev <!-- <a href="http://w3layouts.com" target="_blank">w3layouts</a> --></p>
   </div>
 <div class="clear"></div>
   </div>
