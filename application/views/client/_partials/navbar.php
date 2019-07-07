8da7ff928f9fdfa21339bf31d2ca124c81d1d0b5
munir <hel000941@gmail.com>
2019-07-08 03:54:21 +0700

    partial navbar+sidebar

--- application/views/client/_partials/navbar.php
+++ application/views/client/_partials/navbar.php
@@ -4,28 +4,8 @@
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
                 <i class="icon-reorder shaded"></i></a><a class="brand" href="<?php echo base_url('siswa/home'); ?>">Primagama </a>
             <div class="nav-collapse collapse navbar-inverse-collapse">
-                <ul class="nav nav-icons">
-                    <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
-                    <li><a href="#"><i class="icon-eye-open"></i></a></li>
-                    <li><a href="#"><i class="icon-bar-chart"></i></a></li>
-                </ul>
-                <form class="navbar-search pull-left input-append" action="#">
-                <input type="text" class="span3">
-                <button class="btn" type="button">
-                    <i class="icon-search"></i>
-                </button>
-                </form>
+
                 <ul class="nav pull-right">
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-                        <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Item No. 1</a></li>
-                            <li><a href="#">Don't Click</a></li>
-                            <li class="divider"></li>
-                            <li class="nav-header">Example Header</li>
-                            <li><a href="#">A Separated link</a></li>
-                        </ul>
-                    </li>
                     <?php $jbtn = $this->session->jabatan; ?>
                     <li><a href="#"><?php foreach ($siswa as $m) {
                       // echo ucwords($m->nama_panggilan);
@@ -45,12 +25,73 @@
                       }
                     } ?> </a></li>
                     <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="<?php echo base_url('images/foto/profile/siswa/default.png'); ?>" class="nav-avatar" />
+                      <?php foreach ($siswa as $m) {
+                        // echo ucwords($m->nama_panggilan);
+                        switch ($jbtn) {
+                          case 'siswa':
+                          $ft = $m->foto;
+                            ?><img src="<?php echo base_url('images/foto/profile/siswa/'.$ft); ?>" class="nav-avatar" /><?php
+                            break;
+                          case 'orangtua':
+                            ?><img src="<?php echo base_url('images/foto/profile/siswa/default.png'); ?>" class="nav-avatar" /><?php
+                            break;
+                          case 'tentor':
+                          $ft = $m->foto;
+                            ?><img src="<?php echo base_url('images/foto/profile/tentor/'.$ft); ?>" class="nav-avatar" /><?php
+                            break;
+                          default:
+                            // code...
+                            break;
+                        }
+                      } ?>
+
                         <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Your Profile</a></li>
-                            <li><a href="#">Edit Profile</a></li>
-                            <li><a href="#">Account Settings</a></li>
+                            <li><a href="
+                              <?php foreach ($siswa as $m) {
+                                // echo ucwords($m->nama_panggilan);
+                                switch ($jbtn) {
+                                  case 'siswa':
+                                  $ids = $m->id_siswa;
+                                    echo base_url('siswa/profil/detail/'.encrypt_url($ids));
+                                    break;
+                                  case 'orangtua':
+                                  $ids = $m->id_siswa;
+                                    echo base_url('ortu/profileanak/anak/'.encrypt_url($ids));
+                                    break;
+                                  case 'tentor':
+                                  $idt = $m->id_tentor;
+                                    echo base_url('tentor/profile/detail/'.encrypt_url($idt));
+                                    break;
+                                  default:
+                                    // code...
+                                    break;
+                                }
+                              } ?>
+
+                              ">Your Profile</a></li>
+                            <li><a href="
+                              <?php foreach ($siswa as $m) {
+                                // echo ucwords($m->nama_panggilan);
+                                switch ($jbtn) {
+                                  case 'siswa':
+                                  $ids = $m->id_siswa;
+                                    echo base_url('siswa/setting/index/'.encrypt_url($ids));
+                                    break;
+                                  case 'orangtua':
+                                  $ids = $m->id_siswa;
+                                    echo base_url('ortu/profileanak/anak/'.encrypt_url($ids));
+                                    break;
+                                  case 'tentor':
+                                  $idt = $m->id_tentor;
+                                    echo base_url('tentor/setting/index/'.encrypt_url($idt));
+                                    break;
+                                  default:
+                                    // code...
+                                    break;
+                                }
+                              } ?>
+                              ">Account Settings</a></li>
                             <li class="divider"></li>
                             <li>  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a></li>
                         </ul>
