adc75a29292b81c5ca742678e97356f3e9a73efd
Firmansyah1212 <44762777+Firmansyah1212@users.noreply.github.com>
2019-07-09 00:12:47 +0700

    lg

--- application/views/login.php
+++ application/views/login.php
@@ -1,95 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <title>Login Primagama</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login Primagama</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 <!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
-   
-   <div class="login-form">
-    <?php if ($this->session->flashdata('gagal')): ?>
-    <div class="alert alert-danger" role="alert">
-        <?php echo $this->session->flashdata('gagal'); ?>
+  
+  <div class="limiter">
+    <div class="container-login100" style="background-image: url('images/bg1.jpg');">
+      <div class="wrap-login100 p-t-190 p-b-30">
+        <form class="login100-form validate-form">
+          <div class="login100-form-avatar">
+            <img src="images/user.png" alt="AVATAR">
+          </div>
+
+          <span class="login100-form-title p-t-20 p-b-45">
+            Login Primagama
+          </span>
+
+          <div class="wrap-input100 validate-input m-b-10" data-validate = "Username is required">
+            <input class="input100" type="text" name="username" placeholder="Username">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-user"></i>
+            </span>
+          </div>
+
+          <div class="wrap-input100 validate-input m-b-10" data-validate = "Password is required">
+            <input class="input100" type="password" name="pass" placeholder="Password">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+              <i class="fa fa-lock"></i>
+            </span>
+          </div>
+
+          <div class="container-login100-form-btn p-t-10">
+            <button class="login100-form-btn">
+              Login
+            </button>
+          </div>
+</form>
+         
+          </div>
+        </form>
+      </div>
     </div>
-    <?php endif; ?>
-    <form action="<?php echo base_url('login/login') ?>" method="post">
-       
+  </div>
+  
+  
 
   
-    
-    
-        <div class="container-login100">    
-           
-                <div  class="login100-pic js-tilt" data-tilt>
-                 <img src="images/img-01.png" alt="IMG">
-
-                </div>
-                 <form class="login100-form validate-form">
-                    <span class="login100-form-title">
-                        Primagama Login
-                    </span>
-  <div class="form-group">
-            <input name="username" type="text" class="form-control" placeholder="Username" required="required">
-            <div class="form-group">
-            <input name="password" type="password" class="form-control" placeholder="Password" required="required">
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-block">Log in</button>
-        </div>
-        </div>
-
-
-                
-                   
-        
-    </form>
-
-</div>
-</div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    
-    
-
-    
 <!--===============================================================================================-->  
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-    <script src="vendor/bootstrap/js/popper.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-    <script src="vendor/select2/select2.min.js"></script>
+  <script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-    <script src="vendor/tilt/tilt.jquery.min.js"></script>
-    <script >
-        $('.js-tilt').tilt({
-            scale: 1.1
-        })
-    </script>
-<!--===============================================================================================-->
-    <script src="js/main.js"></script>
-
+  <script src="js/main.js"></script>
 
 </body>
-</html>     
+</html>
