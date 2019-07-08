f096f95b4df55ed9f64eaee583638c0bc5a60e4a
Alex Rudi Herlambang <alexrudiherlambang98@gmail.com>
2019-07-08 12:53:18 +0700

    send wa

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -14,44 +14,12 @@
 
     public function kirimEmail() {
 
-      // Konfigurasi email
-        $config = [
-               'mailtype'  => 'html',
-               'charset'   => 'utf-8',
-               'protocol'  => 'smtp',
-               'smtp_host' => 'ssl://smtp.gmail.com',
-               'smtp_user' => 'alexrudiherlambang98@gmail.com',    // Ganti dengan email gmail kamu
-               'smtp_pass' => 'gakbolehsama,24101998',      // Password gmail kamu
-               'smtp_port' => 465,
-               'crlf'      => "\r\n",
-               'newline'   => "\r\n"
-           ];
+      $pengirim = $this->input->post('email');
+      
+      $pesan = $this->input->post('pesan');
+      $nama = $this->input->post('nama');
 
-        // Load library email dan konfigurasinya
-        $this->load->library('email', $config);
-
-        // Email dan nama pengirim
-        $this->email->from('no-reply@masrud.com', 'MasRud.com | M. Rudianto');
-
-        // Email penerima
-        $this->email->to('penerima@gmail.com'); // Ganti dengan email tujuan kamu
-
-        // Lampiran email, isi dengan url/path file
-        $this->email->attach('https://masrud.com/content/images/20181215150137-codeigniter-smtp-gmail.png');
-
-        // Subject email
-        $this->email->subject('Kirim Email dengan SMTP Gmail | MasRud.com');
-
-        // Isi email
-        $this->email->message("Ini adalah contoh email CodeIgniter yang dikirim menggunakan SMTP email Google (Gmail).<br><br> Klik <strong><a href='https://masrud.com/post/kirim-email-dengan-smtp-gmail' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
-
-        // Tampilkan pesan sukses atau error
-        if ($this->email->send()) {
-            echo 'Sukses! email berhasil dikirim.';
-        } else {
-            echo 'Error! email tidak dapat dikirim.';
-        }
-
+     redirect('https://api.whatsapp.com/send?phone=6282316285715&text=Saya%20'.$nama.'%20Menghubungi%20Primagama%20Bondowoso%20di%20dikarenakan%20'.$pesan);
     }
   }
  ?>
--- application/views/profile/contact.php
+++ application/views/profile/contact.php
@@ -42,10 +42,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		    </div>
 			<p>Jika ingin menghubungi kami, anda bisa mengisi dan melengkapi form berikut:</p>
 		    <div class="contatct-form">
-			   <form method="post" action="<?php echo base_url('profile/kirimEmail') ?>">
+			   <form method="post" action="<?php echo base_url('profile/kirimEmail') ?>" target="_blank">
 				  <input type="text" value="Name " name="nama" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name ';}">
 				  <input type="text" value="Email " name="email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email ';}">
-				  <input type="text" value="Phone " name="hp" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Phone ';}">
+				 
 				  <textarea rows="2" cols="70" name="pesan" onfocus="if(this.value == 'Message ') this.value='';" onblur="if(this.value == '') this.value='Message ';">Message </textarea>
 				  <input type="submit" value="Submit">
 			   </form>
@@ -58,8 +58,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						   		<p>Dabasah, Kec. Bondowoso, Kabupaten Bondowoso</p>
 						   		<p>Jawa Timur</p>
 				   		<p>Phone:0852-3661-0634</p>
-				 	 	<p>Email: <span>info[at]mycompany.com</span></p>
-				   		<p>Follow on: <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fid-id.facebook.com%2Fprimagama.bondowoso"><span>Facebook</span></p>
+				 	 	<p>Email: <span>primagamabondowoso@gmail.com</span></p>
+				   		<p>Follow on: <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fid-id.facebook.com%2Fprimagama.bondowoso"><span>Primagama Bondowoso</span></p>
 				   </div>
 		      </div>
 		     <div class="clear"></div>
@@ -67,64 +67,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		 </div>
 	 </div>
 	 
-     <div class="footer">
-		<div class="wrap">
-			<div class="footer-top">
-				<div class="col_1_of_footer span_1_of_footer">
-					<h3>Beranda</h3>
-					<p> Home </p>
-					<p>Kontak</p>
-				</div>
-				<div class="col_1_of_footer span_1_of_footer">
-					<h3>Home</h3>
-					<p> Primagama Siap </p>
-					<p>Kenapa Primagama</p>
-				</div>
-				<div class="col_1_of_footer span_1_of_footer">
-					<h3> Recent Testimoni </h3>
-					<div class="recent-tweet">
-						<div class="recent-tweet-icon">
-							<span> </span>
-						</div>
-						<div class="recent-tweet-info">
-							<p>Ds which don't look even slightly believable. If you are going to use nibh euismod tincidunt ut laoreet<a href="#">About 1 Hour Ago</a></p>
-						</div>
-						<div class="clear"> </div>
-					</div>
-					<div class="recent-tweet">
-						<div class="recent-tweet-icon">
-							<span> </span>
-						</div>
-						<div class="recent-tweet-info">
-							<p>Ds which don't look even slightly believable. If you are going to use nibh euismod tincidunt ut laoreet<a href="#">About 2 Hour Ago</a></p>
-						</div>
-						<div class="clear"> </div>
-					</div>
-				</div>
-				<div class="col_1_of_footer span_1_of_footer">
-					<h3>Tokoh</h3>
-					<div class="gallery">
-						<ul>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g1.jpg')?>" alt=""/></a></li>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g2.jpg')?>" alt=""/></a></li>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g3.jpg')?>" alt=""/></a></li>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g4.jpg')?>" alt=""/></a></li>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g5.jpg')?>" alt=""/></a></li>
-							<li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="<?php echo base_url('images/g6.jpg')?>" alt=""/></a></li>
-							 <div id="small-dialog1" class="mfp-hide">
-								<div class="pop_up">
-								 	<img src="<?php echo base_url('images/g_zoom.jpg')?>" alt=""/>
-									<h2>Photo View</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-					  			</div>
-							</div>
-						</ul>
-					</div>
-				</div>
-				<div class="clear"></div>
-			</div>
-		</div>
-	    <?php $this->load->view('profile/_partials/foot') ?>
-	</div>
+     
 </body>
 </html>
